15583f342701c1663a2411e5de18aa531bcc3771
Prinsium <prinsium@proton.me>
2025-07-30 12:13:10 +0530

    30

---
+++ src/app/db/market.js
@@ -0,0 +1,34 @@
+const market = [
+  {
+    id: 1,
+    title: "Monomania by True Grit Texture Supply",
+    price: 23,
+  },
+  {
+    id: 2,
+    title: "Wanderer by Frankentoon",
+    price: 19,
+  },
+  {
+    id: 3,
+    title: "Lightplay Overlays by Martins Ribeiro",
+    price: 22,
+  },
+  {
+    id: 4,
+    title: "3D Letters Toolkit by Aurelie Maron",
+    price: 27,
+  },
+  {
+    id: 5,
+    title: "Letter & Grid Builder Bundle by Ian Barnard and Stefan Kunz",
+    price: 23,
+  },
+  {
+    id: 6,
+    title: "HuemanTone LUT Pack by Ivan Weiss",
+    price: 19,
+  },
+];
+
+export default market;
